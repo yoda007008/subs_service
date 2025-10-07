c6bb875a886c0e8875be227bd89f2b0626261ac6
Kirill <masteryoda01010101@gmail.com>
2025-10-07 16:08:19 +0300

    update dto

--- subservice/internal/dto/dto.go
+++ subservice/internal/dto/dto.go
@@ -1,18 +1,33 @@
 package dto
 
-import "time"
+import (
+	"database/sql"
+	"github.com/google/uuid"
+)
 
+// для удобства тестирования функционала приложения
 type SubsMethods interface {
 	CreateSubscription()
 	UpdateSubscription(id int)
 	GetSubscription()
-	DeleteSubscription()
+	DeleteSubscription(id int)
 }
 
 type Subscription struct {
-	Name       string    `json:"service_name"`
-	Price      int       `json:"price"`
-	UserID     int       `json:"user_id"`
-	StartDate  time.Time `json:"start_date"`
-	EndingDate time.Time `json:"ending_ate"` // todo опционально
+	ID          uuid.UUID     `db:"id" json:"id"`
+	ServiceName string        `db:"service_name" json:"service_name"`
+	Price       int           `db:"price" json:"price"`
+	UserID      uuid.UUID     `db:"user_id" json:"user_id"`
+	StartYear   int           `db:"start_year" json:"start_year"`
+	StartMonth  int           `db:"start_month" json:"start_month"`
+	EndYear     sql.NullInt64 `db:"end_year" json:"end_year,omitempty"`
+	EndMonth    sql.NullInt64 `db:"end_month" json:"end_month,omitempty"`
+}
+
+type CreateSubscriptionRequest struct {
+	ServiceName string `json:"service_name"`
+	Price       int    `json:"price"`
+	UserID      string `json:"user_id"`
+	StartDate   string `json:"start_date"`
+	EndDate     string `json:"end_date,omitempty"`
 }
