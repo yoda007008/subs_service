d9a64a8a8ab818a5571f4e09cbfea21d083241ba
Kirill <masteryoda01010101@gmail.com>
2025-10-07 17:37:38 +0300

    update migration

--- subservice/internal/migrations/1_create_sub_table.up.sql
+++ subservice/internal/migrations/1_create_sub_table.up.sql
@@ -4,5 +4,8 @@ CREATE TABLE IF NOT EXISTS "subscriptions" (
     price INTEGER NOT NULL,
     user_id UUID NOT NULL,
     start_year INTEGER NOT NULL,
-    start_month INTEGER NOT NULL
+    end_year INTEGER,
+    start_month INTEGER NOT NULL,
+    end_month INTEGER,
+    created_at DATE
 );
